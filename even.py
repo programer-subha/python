b3dd7c4dadc2c50368a243767b25ace443102b9c
programer-subha <147228526+programer-subha@users.noreply.github.com>
2025-02-08 17:05:13 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,12 @@
+number=int(input("enter an integer number:"))
+remainder=number%2
+if(remainder==0):
+ {
+    print("it is a even number")
+}
+else :
+ {
+    
+    print("it is an odd number")
+}
+ elif
